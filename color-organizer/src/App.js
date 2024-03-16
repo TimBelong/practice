9fea19ff52e99bcd4588b5109498ac405d765f95
inne <mrbelongtim@gmail.com>
2024-03-16 14:50:04 +0100

    added all components for color organizer

--- color-organizer/src/App.js
+++ color-organizer/src/App.js
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
 import colorData from "./color-data.json";
 import ColorList from "./colorList";
+import AddColorForm from "./AddColorForm";
 
 export default function App() {
-  const [colors, setColors] = useState(colorData);
   return (
-    <ColorList 
-      colors={colors} 
-      onRateColor={ (id, rating) => {
-        const newColors = colors.map(color => 
-          color.id === id ? {...color, rating} : color
-        );
-        setColors(newColors);
-      }}
-      onRemoveColor={id => {
-        const newColors = colors.filter(color => color.id !== id);
-        setColors(newColors)
-      }}
-    />
+    <>
+      <AddColorForm />
+      <ColorList />
+    </>
   );
 }
--- color-organizer/src/colorList.js
+++ color-organizer/src/colorList.js
@@ -1,14 +1,17 @@
 import React from "react";
-import Color from './Color';
+import Color from "./Color";
+import { useColors } from "./ColorProvider";
 
-export default function ColorList({colors = [], onRemoveColor = f => f, onRateColor = f => f}){
-    if(!colors.length) return <div>No colors listed.</div>
-    return (
-        <div>
-            {colors.map(color => (
-                <Color key={color.id} {...color} onRemove={onRemoveColor} onRate={onRateColor}/>
-            ))}
-        </div>
-    )
+export default function ColorList() {
+  const { colors } = useColors();
+
+  if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
+
+  return (
+    <div>
+      {colors.map(color => (
+        <Color key={color.id} {...color} />
+      ))}
+    </div>
+  );
 }
-
